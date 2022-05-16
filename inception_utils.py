Corrected weighting and numpy FID quick-fix.

inception_utils.py
@@ -283,7 +283,7 @@ def prepare_inception_metrics(dataset, parallel, no_fid=False):
   # Load network
   net = load_inception_net(parallel)
   def get_inception_metrics(sample, num_inception_images, num_splits=10, 
-                            prints=True, use_torch=True, return_IS_scores=False):
+                            prints=True, use_torch=False, return_IS_scores=False):
     if prints:
       print('Gathering activations...')
     pool, logits, labels = accumulate_inception_activations(sample, net, num_inception_images)
@@ -305,7 +305,7 @@ def prepare_inception_metrics(dataset, parallel, no_fid=False):
         FID = torch_calculate_frechet_distance(mu, sigma, torch.tensor(data_mu).float().cuda(), torch.tensor(data_sigma).float().cuda())
         FID = float(FID.cpu().numpy())
       else:
-        FID = numpy_calculate_frechet_distance(mu.cpu().numpy(), sigma.cpu().numpy(), data_mu, data_sigma)
+        FID = numpy_calculate_frechet_distance(mu, sigma, data_mu, data_sigma)
     # Delete mu, sigma, pool, logits, and labels, just in case
     del mu, sigma, pool, logits, labels
     if return_IS_scores:

losses.py
@@ -14,11 +14,11 @@ def loss_dcgan_gen(dis_fake):
 
 
 # Hinge Loss
-def loss_hinge_dis(dis_fake, dis_real, gy, dy, class_weights):
-  gy_weighting = torch.index_select(class_weights, 0, gy)
-  dy_weighting = torch.index_select(class_weights, 0, dy)
-  loss_real = torch.mean(dy_weighting * F.relu(1. - dis_real))
-  loss_fake = torch.mean(gy_weighting * F.relu(1. + dis_fake))
+def loss_hinge_dis(dis_fake, dis_real, y_fake, y_real, class_weights):
+  y_fake_weighting = torch.index_select(class_weights, 0, y_fake)
+  y_real_weighting = torch.index_select(class_weights, 0, y_real)
+  loss_real = torch.mean(y_real_weighting * F.relu(1. - dis_real))
+  loss_fake = torch.mean(y_fake_weighting * F.relu(1. + dis_fake))
   return loss_real, loss_fake
 # def loss_hinge_dis(dis_fake, dis_real): # This version returns a single loss
   # loss = torch.mean(F.relu(1. - dis_real))
@@ -26,9 +26,9 @@ def loss_hinge_dis(dis_fake, dis_real, gy, dy, class_weights):
   # return loss
 
 
-def loss_hinge_gen(dis_fake, y, class_weights):
-  y_weighting = torch.index_select(class_weights, 0, y)
-  loss = -torch.mean(y_weighting * dis_fake)
+def loss_hinge_gen(dis_fake, y_fake, class_weights):
+  y_fake_weighting = torch.index_select(class_weights, 0, y_fake)
+  loss = -torch.mean(y_fake_weighting * dis_fake)
   return loss
 
 # Default to hinge loss

train.py
@@ -187,7 +187,12 @@ def run(config):
   fixed_y.sample_()
   # Loaders are loaded, prepare the training function
   if config['which_train_fn'] == 'GAN':
-    class_weights = torch.load('/ptmp/wero/class_weights.pt')
+    cwd = os.getcwd()
+    filename = config['class_weights_file']
+    path = os.path.join(cwd, filename)
+    if hvd.rank() == 0:
+      print('Using %s as weights.' % filename)
+    class_weights = torch.load(filename)
     class_weights = class_weights.to(device)
     train = train_fns.GAN_training_function(G, D, GD, z_, y_, 
                                             ema, state_dict, config, class_weights)

utils.py
@@ -37,6 +37,8 @@ def prepare_parser():
   usage = 'Parser for all scripts.'
   parser = ArgumentParser(description=usage)
   
+  parser.add_argument(
+    '--class_weights_file', type=str, default='ecoset_class_weights.pt')
   ### Dataset/Dataloader stuff ###
   parser.add_argument(
     '--dataset', type=str, default='I128_hdf5',
@@ -1111,9 +1113,11 @@ def get_SVs(net, prefix):
             float(d[key].item())
             for key in d if 'sv' in key}
 
+def name_from_config(config):
+  return '_'.join(['weighting', config['class_weights_file']])
 
 # Name an experiment based on its config
-def name_from_config(config):
+def name_from_config_old(config):
   name = '_'.join([
   item for item in [
   'Big%s' % config['which_train_fn'],
@@ -1202,7 +1206,9 @@ def sample_1hot(batch_size, num_classes, device='cuda'):
 class Distribution(torch.Tensor):
   # Init the params of the distribution
   def init_distribution(self, dist_type, **kwargs):    
-    class_weights = torch.load('/ptmp/wero/class_weights.pt').tolist()
+    cwd = os.getcwd()
+    filepath = os.path.join(cwd, 'ecoset_class_counts.pt')
+    class_weights = torch.load(filepath).tolist()
     self.dist_type = dist_type
     self.dist_kwargs = kwargs
     if self.dist_type == 'normal':